b62b012b372bd78ef43505ed3dde5f2c60bf6b16
ppedvLukasK <lukask@ppedv.de>
2024-09-24 11:23:40 +0200

    M004

--- M000/Program.cs
+++ M000/Program.cs
@@ -1,21 +1,17 @@
-﻿int meter;
-string eingabe = Console.ReadLine();
-meter = int.Parse(eingabe);
+﻿int jahr = 2000;
+bool istSchaltjahr = false;
+if (jahr % 4 == 0)
+	istSchaltjahr = true;
+if (jahr % 100 == 0)
+	istSchaltjahr = false;
+if (jahr % 400 == 0)
+	istSchaltjahr = true;
+Console.WriteLine($"{jahr} ist {(istSchaltjahr ? "ein" : "kein")} Schaltjahr");
 
-int stunde;
-eingabe = Console.ReadLine();
-stunde = int.Parse(eingabe);
-
-int minute;
-eingabe = Console.ReadLine();
-minute = int.Parse(eingabe);
-
-int sekunde;
-eingabe = Console.ReadLine();
-sekunde = int.Parse(eingabe);
-
-double gesamt = stunde * 3600 + minute * 60 + sekunde;
-
-Console.WriteLine($"Meter/Sekunde:\t\t{Math.Round(meter / gesamt, 2)}");
-Console.WriteLine($"Kilometer/Stunde:\t{Math.Round((meter / 1000.0) / (gesamt / 3600), 2)}");
-Console.WriteLine($"Meilen/Stunde:\t\t{Math.Round(((meter / 1000.0) / (gesamt / 3600)) * 0.616, 2)}");
+string eingabe = "12";
+int[] gewinnzahlen = [22, 19, 57, 39, 52];
+int zahl = int.Parse(eingabe);
+if (zahl > 0 && zahl < 100)
+    Console.WriteLine(gewinnzahlen.Contains(zahl) ? "Glückwunsch" : "Kein Gewinn");
+else
+    Console.WriteLine("Zahl außerhalb des gültigen Bereiches");
---
+++ M004/Program.cs
@@ -0,0 +1,107 @@
+﻿#region Schleifen
+//while-Schleife
+//Führt die Schleife aus, solange die Bedingung true ist
+using System.Net;
+
+int a = 0;
+int b = 10;
+while (a < b)
+{
+    Console.WriteLine($"while: {a}");
+	a++;
+}
+
+int c = 0;
+int d = 10;
+do
+{
+    Console.WriteLine($"do-while: {c}");
+	c++;
+}
+while (c < d);
+
+//Endlosschleife
+while (true)
+{
+	break;
+}
+
+int e = 0;
+int f = 10;
+while (true)
+{
+    Console.WriteLine(e);
+	e++;
+
+	if (e >= f)
+		break;
+}
+
+//Snippets
+for (int i = 0; i < 10; i++)
+{
+    
+}
+
+for (int i = 9; i >= 0; i--)
+{
+		
+}
+#endregion
+
+#region Enum
+//Enum
+//Liste von Konstanten
+//Zusätzlich ein eigener Typ
+//WICHTIG: Eigene Enums am Ende der Datei anlegen
+
+//String: Unsicher, Fehleranfällig
+string tag = "Di";
+if (tag == "DI")
+{
+
+}
+
+//Enum: Sicher
+Wochentag x = Wochentag.Di;
+if (x == Wochentag.Di)
+{
+
+}
+
+//Weitere Enum-Funktionen
+
+//Parse: Konvertiert Strings und Zahlen innerhalb von Strings zu dem gegebenen Enumtypen
+//WICHTIG: Typ in der spitzen Klammer angeben
+Wochentag parsed = Enum.Parse<Wochentag>(tag);
+Console.WriteLine(parsed);
+
+//GetValues: Alle Werte aus einem Enum in ein Array entnehmen
+Wochentag[] tage = Enum.GetValues<Wochentag>();
+foreach (Wochentag w in tage)
+{
+    Console.WriteLine(w);
+}
+
+//IsDefined: Prüft, ob ein gegebener Enumwert, in dem entsprechenden Enum existiert
+Wochentag wt = (Wochentag) 10;
+if (Enum.IsDefined<Wochentag>(wt)) //Ist in der Variable ein valider Wert enthalten?
+	Console.WriteLine(wt);
+#endregion
+
+#region Switch
+switch(wt)
+{
+	case >= Wochentag.Mo and <= Wochentag.Fr:
+        Console.WriteLine("Wochentag");
+		break;
+	case Wochentag.Sa or Wochentag.So:
+        Console.WriteLine("Wochenende");
+		break;
+}
+#endregion
+
+enum Wochentag
+{
+	Mo = 1, Di, Mi, Do, Fr, Sa, So
+}
