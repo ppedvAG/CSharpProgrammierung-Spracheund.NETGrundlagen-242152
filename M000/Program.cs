558e3fa0daf091264ec78a35690680723926d56d
ppedvLukasK <lukask@ppedv.de>
2024-09-24 10:47:35 +0200

    M003

---
+++ M000/Program.cs
@@ -0,0 +1,21 @@
+﻿int meter;
+string eingabe = Console.ReadLine();
+meter = int.Parse(eingabe);
+
+int stunde;
+eingabe = Console.ReadLine();
+stunde = int.Parse(eingabe);
+
+int minute;
+eingabe = Console.ReadLine();
+minute = int.Parse(eingabe);
+
+int sekunde;
+eingabe = Console.ReadLine();
+sekunde = int.Parse(eingabe);
+
+double gesamt = stunde * 3600 + minute * 60 + sekunde;
+
+Console.WriteLine($"Meter/Sekunde:\t\t{Math.Round(meter / gesamt, 2)}");
+Console.WriteLine($"Kilometer/Stunde:\t{Math.Round((meter / 1000.0) / (gesamt / 3600), 2)}");
+Console.WriteLine($"Meilen/Stunde:\t\t{Math.Round(((meter / 1000.0) / (gesamt / 3600)) * 0.616, 2)}");
---
+++ M003/Program.cs
@@ -0,0 +1,42 @@
+﻿#region Arrays
+int[] zahlen = new int[5];
+zahlen[2] = 10;
+Console.WriteLine(zahlen[2]);
+
+//Direkte Initialisierung
+zahlen = new int[5] { 0, 0, 10, 0, 0 };
+zahlen = new[] { 0, 0, 10, 0, 0 };
+int[] z2 = { 0, 0, 10, 0, 0 };
+int[] z3 = [0, 0, 10, 0, 0]; //Ab .NET 8/C# 12
+
+//Ist ein gegebenes Element in der Liste enthalten?
+Console.WriteLine(zahlen.Contains(5)); //false
+Console.WriteLine(zahlen.Contains(10)); //true
+
+Console.WriteLine(zahlen.Length); //5
+
+//Mehrdimensionales Array
+//Mit Komma in der Arrayklammer definiert
+int[,] matrix = new int[3, 3];
+matrix[0, 0] = 10;
+#endregion
+
+#region Bedingungen
+
+int zahl1 = 12;
+int zahl2 = 7;
+
+if (zahl1 < zahl2) //Einzeilige Ifs können ohne Klammern definiert werden
+    Console.WriteLine("Zahl1 kleiner als Zahl2");
+else
+    Console.WriteLine("Zahl1 ist nicht kleiner als Zahl2");
+
+//Ternary-Operator (?-Operator): Mehrzeilige if/else Blöcke einzeilig machen
+Console.WriteLine(zahl1 < zahl2 ? "Zahl1 kleiner als Zahl2" : "Zahl1 ist nicht kleiner als Zahl2");
+
+//Der Ternary-Operator muss immer ein Ergebnis zurückgeben (nicht void)
+//zahl1 < zahl2 ? Console.WriteLine("Zahl1 kleiner als Zahl2") : Console.WriteLine("Zahl1 ist nicht kleiner als Zahl2");
+
+string ausgabe = zahl1 < zahl2 ? "Zahl1 kleiner als Zahl2" : "Zahl1 ist nicht kleiner als Zahl2";
+Console.WriteLine(ausgabe);
+#endregion
